b7eb3f54ae2dc6c7db9e9edf552f58825f1f5361
hyebinpark <hoonywhitesocks@gmail.com>
2026-02-05 04:14:25 +0900

    feat: Link와 router.push를 활용한 네비게이션

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,10 +1,29 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const onClickButton = () => {
+    router.push('/test');
+    // useRouter의 push 메서드를 사용해
+    // 버튼 클릭 이벤트로 /test 페이지로 이동한다 (클라이언트 사이드 라우팅)
+  };
+
   return (
     <>
-      <header>Header</header>
+      <header>
+        <Link href={'/'}>Logo</Link>
+        &nbsp;
+        <Link href={'/search'}>search</Link>
+        &nbsp;
+        <Link href={'/book/1'}>book/1</Link>
+        <div>
+          <button onClick={onClickButton}>/test 페이지로 이동</button>
+        </div>
+      </header>
       <Component {...pageProps} />
     </>
   );
