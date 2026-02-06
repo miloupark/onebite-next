c8c096fed7cc2870f978d338f6ee29b7db51b01e
hyebinpark <hoonywhitesocks@gmail.com>
2026-02-06 19:09:39 +0900

    feat: 글로벌 레이아웃 적용

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,37 +1,14 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  const onClickButton = () => {
-    router.push('/test');
-    // useRouter의 push 메서드를 사용해
-    // 버튼 클릭 이벤트로 /test 페이지로 이동한다 (클라이언트 사이드 라우팅)
-  };
-
-  useEffect(() => {
-    router.prefetch('/test');
-  }, []);
-
   return (
-    <>
-      <header>
-        <Link href={'/'}>Logo</Link>
-        &nbsp;
-        <Link href={'/search'} prefetch={false}>
-          search
-        </Link>
-        &nbsp;
-        <Link href={'/book/1'}>book/1</Link>
-        <div>
-          <button onClick={onClickButton}>/test 페이지로 이동</button>
-        </div>
-      </header>
-      <Component {...pageProps} />
-    </>
+    <div>
+      <header>Header</header>
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <footer>Footer</footer>
+    </div>
   );
 }
