48e68068052f6dca86cd956b49e394cbb1395e98
hyebinpark <hoonywhitesocks@gmail.com>
2026-02-06 21:35:31 +0900

    feat: 검색 인풋 추가 및 쿼리 스트링 기반 페이지 이동 구현

--- section02/src/components/searchable-layout.tsx
+++ section02/src/components/searchable-layout.tsx
@@ -1,13 +1,33 @@
-import { ReactNode } from 'react';
+import { useRouter } from 'next/router';
+import { ReactNode, useState } from 'react';
 
 export default function SearchableLayout({
   children,
 }: {
   children: ReactNode;
 }) {
+  const router = useRouter();
+  const [search, setSearch] = useState('');
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onSubmit = () => {
+    if (!search) return;
+    router.push(`/search?q=${search}`);
+  };
+
   return (
     <div>
-      <div>Searchbar</div>
+      <div>
+        <input
+          value={search}
+          placeholder='검색어를 입력하세요'
+          onChange={onChangeSearch}
+        />
+        <button onClick={onSubmit}>검색</button>
+      </div>
       {children}
     </div>
   );
