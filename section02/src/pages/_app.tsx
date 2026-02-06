ab7715c34676212f8595b8377cffafd8c5c54cf5
hyebinpark <hoonywhitesocks@gmail.com>
2026-02-06 20:41:38 +0900

    feat: 글로벌 레이아웃 컴포넌트 분리 및 스타일링 적용

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,14 +1,11 @@
+import GlobalLayout from '@/components/global-layout';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
-      <header>Header</header>
-      <main>
-        <Component {...pageProps} />
-      </main>
-      <footer>Footer</footer>
-    </div>
+    <GlobalLayout>
+      <Component {...pageProps} />
+    </GlobalLayout>
   );
 }
