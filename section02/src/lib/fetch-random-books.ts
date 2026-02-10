e1910b5b4fadbfb8799e6a22f57ba1205d674265
hyebinpark <hoonywhitesocks@gmail.com>
2026-02-10 22:18:46 +0900

    feat: SSR에서 추천 도서 랜덤 API 데이터 페칭 추가

---
+++ section02/src/lib/fetch-random-books.ts
@@ -0,0 +1,18 @@
+import { BookData } from '@/types';
+
+// 랜덤 도서 불러오기 (추천 도서)
+export default async function fetchRandomBooks(): Promise<BookData[]> {
+  const url = `http://localhost:12345/book/random`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error();
+    }
+
+    return await response.json();
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
+}
--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,29 +1,32 @@
 import SearchableLayout from '@/components/searchable-layout';
 import style from './index.module.css';
 import { ReactNode } from 'react';
-import books from '@/mock/books.json';
 import BookItem from '@/components/book-items';
 import { InferGetServerSidePropsType } from 'next';
 import fetchBooks from '@/lib/fetch-books';
+import fetchRandomBooks from '@/lib/fetch-random-books';
 
 export const getServerSideProps = async () => {
   const allBooks = await fetchBooks();
+  const recoBooks = await fetchRandomBooks();
 
   return {
     props: {
       allBooks,
+      recoBooks,
     },
   };
 };
 
 export default function Home({
   allBooks,
+  recoBooks,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
+        {recoBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
