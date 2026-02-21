8da1db753a313ab23e4a53de3ab7c5d8868a4810
hyebinpark <hoonywhitesocks@gmail.com>
2026-02-22 02:25:17 +0900

    feat: 도서 검색 API 페칭 로직 추가

--- section04/src/app/(with-searchbar)/search/page.tsx
+++ section04/src/app/(with-searchbar)/search/page.tsx
@@ -1,13 +1,23 @@
-import books from '@/mock/books.json';
 import BookItem from '@/components/book-item';
+import { BookData } from '@/types';
 
-export default function Page({
+const API_BASE = process.env.NEXT_PUBLIC_API_SERVER_URL;
+
+export default async function Page({
   searchParams,
 }: {
   searchParams: {
     q?: string;
   };
 }) {
+  const response = await fetch(`${API_BASE}/book/search?q=${searchParams.q}`);
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다.</div>;
+  }
+
+  const books: BookData[] = await response.json();
+
   return (
     <div>
       {books.map((book) => (
