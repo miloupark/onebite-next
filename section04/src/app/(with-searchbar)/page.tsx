2902a3090dbe0d07c24ddde87b684c919a5c04a3
hyebinpark <hoonywhitesocks@gmail.com>
2026-02-22 02:13:38 +0900

    feat: 도서 목록 데이터 페칭 구현

--- section04/src/app/(with-searchbar)/page.tsx
+++ section04/src/app/(with-searchbar)/page.tsx
@@ -1,21 +1,53 @@
 import BookItem from '@/components/book-item';
 import style from './page.module.css';
-import books from '@/mock/books.json';
+import { BookData } from '@/types';
+
+// 모든 도서
+async function AllBooks() {
+  const response = await fetch(`http://localhost:12345/book`);
+  if (!response.ok) {
+    return <div>오류가 발생했습니다.</div>;
+  }
+
+  const allBooks: BookData[] = await response.json();
+
+  return (
+    <div>
+      {allBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+
+// 추천 도서
+async function RecommendedBooks() {
+  const response = await fetch(`http://localhost:12345/book/random`);
+  if (!response.ok) {
+    return <div>오류가 발생했습니다.</div>;
+  }
+
+  const recommendedBooks: BookData[] = await response.json();
+
+  return (
+    <div>
+      {recommendedBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
 
 export default function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <RecommendedBooks />
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <AllBooks />
       </section>
     </div>
   );
