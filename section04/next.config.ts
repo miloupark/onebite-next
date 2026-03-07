b18f8d8fa7c78e1bd4c87e7260c978cd248ff83d
hyebinpark <hoonywhitesocks@gmail.com>
2026-03-07 09:29:41 +0900

    chore: fetch 요청 로그 출력 설정 추가

--- section04/next.config.ts
+++ section04/next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // 페칭 발생할 때마다 로그 출력하도록 설정
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
 };
 
 export default nextConfig;
